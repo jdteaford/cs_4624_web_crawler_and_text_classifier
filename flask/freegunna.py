042e721ff8c2d3a4adb97502b5c06bc74be864a7
michelle-yh02 <myhong@vt.edu>
2024-03-27 01:06:00 -0400

    forgot to push backend

--- flask/freegunna.py
+++ flask/freegunna.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template_string
+from flask import Flask, request, jsonify, render_template_string, make_response
 from flask_cors import CORS
 from pymongo import MongoClient
 from pymongo.errors import ServerSelectionTimeoutError
@@ -12,7 +12,7 @@ import requests
 # from bs4 import BeautifulSoup
 # import datetime
 # from operator import itemgetter
-# import heapq
+import heapq
 # import random
 # import json
 # import os
@@ -21,7 +21,9 @@ import requests
 
 
 app = Flask(__name__)
-CORS(app)
+#CORS(app)
+CORS(app, origins='http://localhost:3000')
+
 app.config['SECRET_KEY'] = 'professor_farag'
 jwt = JWTManager(app)
 
@@ -108,10 +110,72 @@ def login():
     else:
         return jsonify({"error": "Invalid username or password"}), 401
     
+
 #////////////////////////////////////////////////////////////////////////////////////////////////////
 #///////////////////////////////////// the wall /////////////////////////////////////////////////////
 #////////////////////////////////////////////////////////////////////////////////////////////////////
+# Priority Queue --------------------------------------------------------------------------------------------------------------
+prio_heap = []
+
+def push_to_heap(urlThing):
+    heapq.heappush(prio_heap, urlThing)
+
+def pop_heap():
+    return heapq.heappop(prio_heap)
+# -----------------------------------------------------------------------------------------------------------------------------
+# Simple class that dictates how URL objects are made and handled.
+class URLobj:
+    def __init__(self,id,url,url_score=1,para_score=1,avg_score=1,pid=None):
+        self.id = id
+        self.url = url
+        self.url_score = url_score
+        self.para_score = para_score
+        self.avg_score = avg_score
+        self.pid = pid
+        self.children = []
+
+    # Less than comparison (<)
+    def __lt__(self, other):
+        return self.avg_score > other.avg_score
+
+    def __str__(self):
+        return f"(URL: {self.url}, ID: {self.id}, Avg Score: {self.avg_score}, PID: {self.pid})"
     
+    # Method to convert URLThing to JSONArray
+    def toJSON(self):
+        return {
+                "ID": self.id, 
+                "URL": self.url, 
+                "URL_Score": self.url_score, 
+                "Para_Score": self.para_score, 
+                "Avg_score":self.avg_score, 
+                "PID": self.pid,
+                "children": [child.toJSON() for child in self.children]
+            } 
+
+    def add_child(self,child):
+        self.children.append(child)
+
+    def remove_child(self,child):
+        self.children.remove(child)
+
+# -----------------------------------------------------------------------------------------------------------------------------
+
+# Function to collect URLs from user input and push URLs to heap.
+def read_urls_from_file(file_name):
+
+    with open(file_name, 'r') as file:
+        # Read the content of the file and split it into individual URLs
+        urls = file.read().split('\n')
+
+        for url in urls:
+            global count
+            hold = URLobj(count,url)
+            push_to_heap(hold) # Given URLs will be assumed as valid
+            crawlJSONArray.append(hold.toJSON())
+            count+=1
+
+  
 keywords = ["Monterey", "Park", "California", "mass", "shooting", "mass", "shooting", "Lunar", "New", "Year", "Los", "Angeles", "mass", "shooting",    "Lunar", "New", "Year", "shooting",    "California", "shooting",    "Mass", "shooting", "victims",    "Monterey", "Park", "mass", "shooter",    "Mayor", "Karen", "Bass",    "Victims", "identified",    "Gunman", "manhunt",    "Police", "search", "motive",    "Back-to-back", "mass", "shootings",    "Suspected", "shooter",    "Gun", "safety",    "LAPD", "response",    "Manifesto",    "Asian", "American", "communities",    "Joe", "Biden", "statement",    "Shooting", "at", "dance", "club",    "Kinship", "with", "U.S.", "city",    "Monterey", "Park", "essay",    "911", "call", "audio",    "Biden's", "executive", "order", "on", "gun", "control", "Shooting", "at", "Chinese", "New", "Year", "celebration",    "General", "news",    "Shooting", "near", "Lunar", "New", "Year", "festival",    "Processing", "a", "tragedy",    "Community", "mourns",    "Georgia", "shooting",    "Active", "shooter",    "Hampton", "Georgia", "shooting",    "Midtown", "Atlanta", "shooting",    "Northside", "Hospital",    "Hospital", "massacre",    "Deion", "Patterson",    "Massacre", "suspect", "charged",    "What", "we", "know", "about", "the", "victims",    "Victims", "of", "mass", "shooting",    "Hospital", "shooting", "in", "Atlanta",    "Mass", "shooting", "reports",    "Canada", "shooting",    "Monterey", "Park", "press", "release",    "National", "news",    "Multiple", "casualties",    "Gunman's", "manifesto",    "Gun", "control",    "Biden", "statement",    "Mass", "shooting", "response",    "Medical", "facility", "shooting",    "University", "of", "North", "Carolina", "shooting","Zijie", "Yan",    "UNC", "shooting", "victim",    "Active", "shooter", "situation",    "West", "Peachtree", "Street",    "Spa", "shootings","Atlanta", "shooting",    "Atlanta", "Midtown", "shooting",    "Suspect", "Deion", "Patterson",    "Mass", "shooting", "on", "loose",    "Policing", "Equity"]
 
 urlThreshold = 0.0
@@ -134,6 +198,57 @@ crawlStatFails = {}
 crawl_id = ""
 
 hubs = 0
+
+
+
+#endpoint that performs crawl
+@app.route('/scrape_and_save', methods = ["POST"])
+def scrape_and_save():
+    print('\n\n\nhit scrape\n\n\n')
+
+    if request.method == 'OPTIONS':
+    # Respond to preflight request
+        response = make_response()
+        response.headers.add('Access-Control-Allow-Origin', 'http://localhost:3000')
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
+        response.headers.add('Access-Control-Allow-Methods', 'POST')
+        return response
+
+    #variables for user hard count caps
+    global userHardCount
+    userHardCount = int(request.form['urlTotal'])
+    global urlThreshold
+    urlThreshold = float(request.form['urlThreshold'])
+    global paraThreshold
+    paraThreshold = float(request.form['paraThreshold'])
+    global pageTotal
+    pageTotal = float(request.form['pageTotal'])
+    global crawlJSONArray
+    
+    #uploaded file.
+    # Accessing files
+    for key in request.files:
+        uploaded_file = request.files[key]
+        filename = uploaded_file.filename
+
+        if filename != '':
+            # Process the file as needed (e.g., save it)
+            uploaded_file.save(filename)
+            print(f"Saved file '{filename}'")
+
+            read_urls_from_file(uploaded_file.filename) 
+
+            global crawl_id
+
+            crawl_id = request.form['crawl_id']
+
+    #for testing
+    print(userHardCount)
+    response_data = {
+        "message": "recieved form data",
+        "data": userHardCount
+    }
+    return jsonify(response_data), 200
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
