6bef3c49b82b81576de74e22ef0dcf2316deef96
sondevra <sondrar@vt.edu>
2024-04-07 14:06:41 -0400

    New visual effects

--- client-final/src/App.js
+++ client-final/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
+import { createUseStyles } from 'react-jss';
 
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
@@ -15,7 +15,34 @@ import CrawlHistory from './pages/CrawlHistory';
 //   return isLoggedIn ? children : <Navigate to="/login" />;
 // }
 
+const useStyles = createUseStyles({
+  movingBackground: {
+    animation: '$moveBackground 10s infinite',
+    background: 'linear-gradient(to bottom right, #ff8d44, #833636, #660000)',
+    height: '100vh',
+    width: '100vw', 
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: -1,
+  },
+  '@keyframes moveBackground': {
+    '0%': {
+      backgroundSize: '100% 100%',
+    },
+    '50%': {
+      backgroundSize: '200% 200%',
+    },
+    '100%': {
+      backgroundSize: '100% 100%',
+    },
+  },
+});
+
 function App() {
+  const classes = useStyles();
 
   // const [authenticated, setAuthenticated] = useState(false);
 
@@ -47,6 +74,9 @@ function App() {
   // }, []);
   
   return (
+    <div className='seperate'>
+      <div className={classes.movingBackground}></div>
+    <div className='router'>
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
@@ -56,6 +86,9 @@ function App() {
         <Route path="/crawlhistory" element={<PrivateRoute><CrawlHistory /></PrivateRoute>}/>
       </Routes>
     </Router>
+    </div>
+    
+    </div>
   );
 }
 
