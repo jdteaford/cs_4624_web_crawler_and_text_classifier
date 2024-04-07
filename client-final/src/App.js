1ba2e9ed1a3c8e0b3eca57339274528b2c78e694
Jordan <jdteaford@vt.edu>
2024-04-07 17:19:16 -0400

    added train route

--- client-final/src/App.js
+++ client-final/src/App.js
@@ -10,6 +10,7 @@ import LandingPage from './pages/Landing';
 import LoginPage from './pages/Login';
 import NewCrawl from './pages/NewCrawl';
 import CrawlHistory from './pages/CrawlHistory';
+import Train from './pages/Train';
 
 // function ProtectedRoute({ children }) {
 //   const { isLoggedIn } = useAuth();
@@ -56,6 +57,7 @@ function App() {
         <Route path="*" element={<Navigate replace to="/" />} />
         <Route path="/newcrawl" element={<PrivateRoute><NewCrawl /></PrivateRoute>}/>
         <Route path="/crawlhistory" element={<PrivateRoute><CrawlHistory /></PrivateRoute>}/>
+        <Route path="/train" element = {<PrivateRoute><Train /></PrivateRoute>} />
       </Routes>
     </Router>
     </div>
